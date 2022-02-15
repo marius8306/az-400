9380eb5f389cea5f6cc0568925be5a0161ee10f2
Alexander Pajer <alexander.pajer@integrations.at>
2022-02-15 08:12:54 +0100

    agent vm

--- Demos/04-AzurePipelines/Demo-06/create-agent-vm.sh
+++ Demos/04-AzurePipelines/Demo-06/create-agent-vm.sh
@@ -1,12 +1,12 @@
-rnd=$RANDOM
+env=vm-agent
 loc=westeurope
-grp=az400-m04-$rnd
-vmname=devops-$rnd
+grp=az400-m04-$env
+vmname=devops-$env
 user=az400admin
 pwd=TiTp4@dmin12334!
 
 az group create -n $grp -l $loc
 
-az vm create -g $grp -n $vmname --admin-username $user --admin-password $pwd --image  MicrosoftWindowsServer:WindowsServer:2022-datacenter-g2:latest --size Standard_E2s_v3
+az vm create -g $grp -n $vmname --admin-username $user --admin-password $pwd --image  MicrosoftWindowsServer:WindowsServer:2022-datacenter-g2:latest --size Standard_E2s_v3 --public-ip-sku Standard
 
 # az vm list-sizes -l $loc -o table
