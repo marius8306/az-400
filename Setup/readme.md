552882aef4412ef34e4bb625cc714474433c10aa
Alexander Pajer <alexander.pajer@integrations.at>
2022-02-25 15:59:10 +0100

    setup

--- Setup/readme.md
+++ Setup/readme.md
@@ -2,17 +2,28 @@
 
 ## Create VM
 
-We are using this image and vm size because it supports nested virtualization used for Hyper-V and WSL2 Support
-
-Execute `create-lab-vm.sh` or run the following remote script in Cloud Shell
+Execute `create-lab-vm.azcli` or run the following remote script in Cloud Shell
 
 ```bash
-curl https://raw.githubusercontent.com/ARambazamba/AZ-400/main/Setup/create-lab-vm.sh | bash
+curl https://raw.githubusercontent.com/arambazamba/az-204/main/Setup/create-lab-vm.azcli | bash
 ```
 
 ![create-labvm](_images/create-lab-vm.jpg)
 
-> Note: In order for Docker to work on a Windows 10 host you need to install Hyper-V or use Windows Subsystem for Linux 2 (WSL2). A detailed Setup Guide can be found [here](https://github.com/ARambazamba/ClassSetup)
+`create-lab-vm.azcli`:
+
+```bash
+rnd=$RANDOM
+loc=westeurope
+grp=az-lab
+vmname=labvm-$rnd
+user=azlabadmin
+pwd=Lab@dmin1234
+
+az group create -n $grp -l $loc
+
+az vm create -g $grp -n $vmname --admin-username $user --admin-password $pwd --image MicrosoftWindowsDesktop:Windows-10:win10-21h2-pro-g2:latest --size Standard_E2s_v3 --public-ip-sku Standard
+```
 
 ## Connect to VM
 
--- Setup/setup-az-400.ps1
+++ Setup/setup-az-400.ps1
@@ -15,20 +15,18 @@ choco install gitextensions -y
 choco install git-lfs.install -y
 choco install nodejs-lts --version=14.18.0 -y
 choco install azure-cli -y
-choco install azure-functions-core-tools --params="'/x64:true'" -y
+choco install azure-functions-core-tools@3 --params="'/x64:true'" -y
 choco install gh -y
 choco install curl -y
 
 # Refresh Path Env for npm 
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 
-# Set Nuget 
+# Set NuGet Source
 dotnet nuget add source https://api.nuget.org/v3/index.json -n nuget.org
 
 # Intall VS Code Extensions
 code --install-extension ms-dotnettools.csharp
-code --install-extension msjsdiag.debugger-for-chrome
-code --install-extension msjsdiag.debugger-for-edge
 code --install-extension ms-vscode.powershell
 code --install-extension ms-vscode.azurecli
 code --install-extension ms-vscode.azure-account
@@ -38,12 +36,16 @@ code --install-extension ms-azuretools.vscode-azurefunctions
 code --install-extension GitHub.vscode-pull-request-github
 code --install-extension redhat.vscode-yaml
 code --install-extension bencoleman.armview
-code --install-extension msazurermtools.azurerm-vscode-tools
-code --install-extension CoenraadS.bracket-pair-colorizer-2
 code --install-extension mdickin.markdown-shortcuts
 code --install-extension mhutchie.git-graph 
 code --install-extension ms-azure-devops.azure-pipelines		
 code --install-extension ms-azuretools.vscode-azureterraform
+code --install-extension vs-publisher-1448185.keyoti-changeallendoflinesequence
+code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
+
+# Azurite Storage Emulator & Function Core Tools v3
+npm install -g azure-functions-core-tools@3 --unsafe-perm true
+npm install -g azurite
 
 # Install Angular
 npx @angular/cli@latest analytics off
