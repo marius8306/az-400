95c7408df9d5f643d3268ec0fc5d10527719afd8
Alexander Pajer <alexander.pajer@integrations.at>
2022-04-04 11:29:05 +0200

    setup script vm update

--- Setup/create-lab-vm.sh
+++ Setup/create-lab-vm.sh
@@ -7,6 +7,6 @@ pwd=Lab@dmin1234
 
 az group create -n $grp -l $loc
 
-az vm create -g $grp -n $vmname --admin-username $user --admin-password $pwd --image  MicrosoftWindowsDesktop:Windows-10:21h1-pro:latest --size Standard_E2s_v3 --public-ip-sku Standard
+az vm create -g $grp -n $vmname --admin-username $user --admin-password $pwd --image  MicrosoftWindowsDesktop:Windows-10:21h1-pro:latest --size Standard_E2s_v5 --public-ip-sku Standard
 
 az vm auto-shutdown -g $grp -n $vmname --time 1730
